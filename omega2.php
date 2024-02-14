50ad7498f2b4a5103e89aa226bcfa1eea8bec23a
chava-PA <159972658+chava-PA@users.noreply.github.com>
2024-02-13 19:29:34 -0600

    Add files via upload

---
+++ omega2.php
@@ -0,0 +1,33 @@
+<html>
+<head></head>
+<body>
+<h1>12655. C09</h1>
+
+<h2>Descripción</h2>
+Escribe un programa que solicite las calificaciones de Matemáticas, 
+Español y Cocina y nos de su promedio.
+
+<h2>Entrada</h2>
+Tres números reales
+
+<h2>Salida</h2>
+Un número real representado el promedio de los tres números
+
+<h2>Ejemplo</h2>
+
+<?php
+# CODIGO PHP PARA LA MULTIPLICACION
+$a=10;
+$b=9.5;
+$c=9.0;
+
+# OPERACION
+$m=$a+$b+$c;
+$v=$m/3;
+
+echo "<br>calificacion final :", $v;
+?>
+
+
+</body>
+</html>
